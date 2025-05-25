9c58ec5289f8bc6a906608bc5ae4f7eefeef64a2
willmasilva.5050@gmail.com <willmasilva.ws@gmail.com>
2025-05-25 19:54:53 -0300

    first

--- src/apiClima.ts
+++ src/apiClima.ts
@@ -2,8 +2,7 @@
 import axios from "axios";
 import type { ClimaCompleto } from "./interfaces/ClimaCompleto";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-
+const API_KEY = "8673413a4d6e40bbba611711252505";
 
 const nomesPermitidos: Record<string, string> = {
   "inglaterra": "London",
@@ -11,23 +10,27 @@ const nomesPermitidos: Record<string, string> = {
   "joao pessoa": "João Pessoa" 
 };
 
-
 export const fetchWeatherData = async (
   cidade: string
 ): Promise<ClimaCompleto> => {
-  const cidadeFormatada = cidade.trim().toLowerCase();
-  const consulta = nomesPermitidos[cidadeFormatada] || cidade;
+  try {
+    const cidadeFormatada = cidade.trim().toLowerCase();
+    const consulta = nomesPermitidos[cidadeFormatada] || cidade;
 
-  const response = await axios.get("https://api.weatherapi.com/v1/forecast.json", {
-    params: {
-      key: API_KEY,
-      q: consulta,
-      days: 3,
-      aqi: "no",
-      alerts: "no",
-      lang: "pt",
-    },
-  });
+    const response = await axios.get("https://api.weatherapi.com/v1/forecast.json", {
+      params: {
+        key: API_KEY,
+        q: consulta,
+        days: 3,
+        aqi: "no",
+        alerts: "no",
+        lang: "pt",
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar dados do clima:", error);
+    throw new Error("Não foi possível obter os dados do clima.");
+  }
 };
